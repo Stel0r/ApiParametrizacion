d2641b3617fefd95b73f216aec19d39289445c66
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2024-05-25 12:28:26 -0500

    first commit

---
+++ src/main/java/FourParks/Api/Parametrizacion/ApiParametrizacion/ApiParametrizacionApplication.java
@@ -0,0 +1,13 @@
+package FourParks.Api.Parametrizacion.ApiParametrizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParametrizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParametrizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FourParks/Api/Parametrizacion/ApiParametrizacion/ApiParametrizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package FourParks.Api.Parametrizacion.ApiParametrizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParametrizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
