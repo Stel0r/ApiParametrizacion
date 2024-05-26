2816e6715471fe00bb3383a8acc94123294240f8
Diego Gamez <diego.felipe.gamez@gmail.com>
2024-05-26 17:30:13 -0500

    Update ParqueaderoRepositorio.java

--- src/main/java/FourParks/Api/Parametrizacion/ApiParametrizacion/Repositorios/ParqueaderoRepositorio.java
+++ src/main/java/FourParks/Api/Parametrizacion/ApiParametrizacion/Repositorios/ParqueaderoRepositorio.java
@@ -41,6 +41,6 @@ public interface ParqueaderoRepositorio extends CrudRepository<Parqueadero,Strin
     @Query(value = "select p.K_COD_PARQUEADERO as codParqueadero,count(K_COD_RESERVA) as cantReservas,p.N_NOMBRE as nombre,sum(Q_SUB_TOTAL) as total from RESERVA r ,Parqueadero p WHERE p.K_COD_PARQUEADERO = r.K_COD_PARQUEADERO and (F_FECHA_RESERVA BETWEEN CURDATE() - INTERVAL 15 DAY AND CURDATE()) and p.K_COD_GERENTE = ? GROUP BY p.K_COD_PARQUEADERO,p.N_NOMBRE",nativeQuery = true)
     public List<Map<String,Object>> crearReporteParqueaderos(String gerente);
 
-    @Query(value = "select r.F_FECHA_RESERVA as fecha, sum(r.Q_SUB_TOTAL) total, count(r.K_COD_RESERVA) as cantReservas from RESERVA r WHERE r.F_FECHA_RESERVA BETWEEN CURDATE() - INTERVAL 15 DAY AND CURDATE() AND r.K_COD_PARQUEADERO = ? GROUP BY r.F_FECHA_RESERVA;",nativeQuery = true)
+    @Query(value = "select r.F_FECHA_RESERVA as fecha, sum(r.Q_SUB_TOTAL) total, count(r.K_COD_RESERVA) as cantReservas , COUNT(distinct K_COD_PUESTO)/p.N_NUM_PUESTOS as mayorOcupacion from RESERVA r, Parqueadero p WHERE r.F_FECHA_RESERVA BETWEEN CURDATE() - INTERVAL 15 DAY AND CURDATE() AND r.K_COD_PARQUEADERO = ? GROUP BY r.F_FECHA_RESERVA",nativeQuery = true)
     public List<Map<String,Object>> crearReporteParqueadero(String parqueadero);
 }
